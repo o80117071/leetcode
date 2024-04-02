dc7d63862c8026d5af782f377bc21c473fd04a6a
o80117071 <o80117071@gmail.com>
2024-04-02 15:08:26 +0800

    Create Squares of a Sorted Array.cpp

---
+++ Squares of a Sorted Array.cpp
@@ -0,0 +1,29 @@
+#時間複雜度 O(n)
+#空間複雜度 O(n)
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> ans(nums.size());
+        int l = 0;
+        int r = nums.size()-1;
+        int pos = nums.size()-1;;
+        
+        while(l<=r)
+        {
+            if(nums[l]*nums[l] > nums[r]*nums[r])
+            {
+                ans[pos] =nums[l]*nums[l];
+                l++;
+            }
+            else
+            {
+                ans[pos] =nums[r]*nums[r];
+                r--;
+            }           
+            pos--;
+        }
+        
+        return ans;
+    }
+};
